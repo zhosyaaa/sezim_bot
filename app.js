52c6accf606436dd0ccf9f2af1777a327edb4ade
zhake <musabecova05@gmail.com>
2024-03-29 22:05:47 +0600

    2

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 3000; // Порт, на котором будет запущен сервер
 
 // Создание экземпляра бота Telegram
-const botToken = '7134028377:AAH0UHbDfjYX_bajNKC5VmYhSuJpXBv9OyE'; // Замените на ваш токен доступа
+const botToken = 'YOUR_BOT_TOKEN_HERE'; // Замените на ваш токен доступа
 const bot = new Telegraf(botToken);
 
 // Настройка обработчика команды /start
@@ -21,13 +21,13 @@ bot.on('text', (ctx) => {
 });
 
 // Создание мидлвара для парсинга JSON
-app.use(bodyParser.text());
+app.use(bodyParser.json());
 
 // Маршрут для обработки данных от ноукод-сайта
 app.post('/webhook', (req, res) => {
     const dataFromNoCode = req.body; // Получаем данные от ноукод-сайта
     console.log('Получены данные от ноукод-сайта:', dataFromNoCode);
-    console.log(req);
+    // console.log(req);
     // Далее вы можете обработать данные и отправить нужные уведомления в ваш телеграм-бот
     res.sendStatus(200); // Отправляем ответ на запрос
 });
